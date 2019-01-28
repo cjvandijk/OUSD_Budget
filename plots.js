e4cdbdf9c9dca4e72b7c33611b14805e0622d508
[claudia van_dijk] <[cj@yourpalclaudia.com]>
2019-01-27 16:37:54 -0800

    POC-complete js chart

--- plots.js
+++ plots.js
@@ -1,21 +1,52 @@
-// YOUR CODE HERE
+// have a quick look at data
 console.log(data);
 
-var trace1 = {
-	y: data.Critical_Rating,
-	x: data.Budget,
-	type: "scatter",
-	mode: "markers"
-	}
+// generate random array for color mapping
+var colorArray = []
+while(colorArray.length < 30){
+    var r = Math.floor(Math.random()*100/3) + 1;
+    if(colorArray.indexOf(r) === -1) colorArray.push(r);
 };
+console.log(colorArray);
 
+// set up the trace element
+var trace1 = {
+	x: data.map(row => row.Budget),
+	y: data.map(row => row.Critical_Rating),
+	text: data.map(row => row.Site),
+	type: "scatter",
+	mode: "markers",
+	marker: {
+		color: colorArray,
+		colorscale: 'Jet',
+		opacity: 0.5,
+		cmin: 0,
+		cmax: 50,
+		line: {
+			color: 'rgba(165, 196, 50, 1)',
+			width: 1,
+		},
+      symbol: 'circle',
+      size: 20
+	}
+  }
 
 // set up the data variable
 var data = [trace1];
 
-// set up the layout variable
+// set up the layout 
 var layout = {
-  title: "OUSD Cost vs. Priority",
+	title: "OUSD Cost vs. Priority",
+	xaxis: {
+		title: {
+			text: 'Cost (Budget)',
+		},
+	},
+	yaxis: {
+		title: {
+			text: 'Priority (via Principals)',
+		}
+	}
 };
 
 // Render the plot to the div tag with id "plot"
